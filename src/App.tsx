cf51a3ac626f1fa27d7886a34f2072ff85115f34
Cristian Araujo de Jesus <cristian.jesus@zup.com.br>
2023-07-26 18:21:16 -0300

    feat: Implement Creating New Notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useState } from "react";
+import { useEditor, EditorContent, Content } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { v4 as uuid } from 'uuid'
 
 import styles from "./App.module.css";
 
+type Note = {
+  id: string;
+  title: string;
+  content: Content;
+  updatedAt: Date;
+}
+
 function App() {
+  const [notes, setNotes] = useState<Record<string, Note>>({})
+
   const editor = useEditor({
     extensions: [StarterKit],
     content: "<p>Hello World!</p>",
@@ -22,12 +33,42 @@ function App() {
     editor?.chain().focus().toggleItalic().run();
   };
 
+  const handleCreateNewNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: 'New note',
+      content: `<h1>New note</h1>`,
+      updatedAt: new Date()
+    }
+
+    setNotes((notes) => ({
+      ...notes,
+      [newNote.id]: newNote
+    }))
+  }
+
+  const notesList = Object.values(notes).sort(
+    (a, b) => b.updatedAt.getTime() - a.updatedAt.getTime()
+  )
+
   return (
     <div className={styles.pageContainer}>
       <aside className={styles.sidebar}>
-        <button>
+        <button className={styles.sidebarButton} onClick={handleCreateNewNote}>
           New Note
         </button>
+        <section className={styles.sidebarList}>
+          {notesList.map((note) => (
+            <div
+              key={note.id}
+              role="button"
+              tabIndex={0}
+              className={styles.sidebarItem}
+            >
+              {note.title}
+            </div>
+          ))}
+        </section>
       </aside>
       <main className={styles.editorContainer}>
         <div className={styles.toolbar}>
