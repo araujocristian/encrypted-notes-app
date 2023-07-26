c494adfdc4c0023c5341aa154664bf2dc73c8018
Cristian Araujo de Jesus <cristian.jesus@zup.com.br>
2023-07-26 17:59:18 -0300

    feat: Build the text Editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+import styles from "./App.module.css";
+
 function App() {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Hello World!</p>",
+    editorProps: {
+      attributes: {
+        class: styles.textEditor,
+      },
+    },
+  });
+
+  const toggleBold = () => {
+    editor?.chain().focus().toggleBold().run();
+  };
+
+  const toggleItalic = () => {
+    editor?.chain().focus().toggleItalic().run();
+  };
+
   return (
-    <div></div>
-  )
+    <div className={styles.pageContainer}>
+      <aside className={styles.sidebar}>
+        <button>
+          New Note
+        </button>
+      </aside>
+      <main className={styles.editorContainer}>
+        <div className={styles.toolbar}>
+          <button
+            className={
+              editor?.isActive("bold")
+                ? styles.toolbarButtonActive
+                : styles.toolbarButton
+            }
+            onClick={toggleBold}
+          >
+            B
+          </button>
+          <button
+            className={
+              editor?.isActive("italic")
+                ? styles.toolbarButtonActive
+                : styles.toolbarButton
+            }
+            onClick={toggleItalic}
+          >
+            I
+          </button>
+        </div>
+        <EditorContent editor={editor} className={styles.textEditorContent} />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
